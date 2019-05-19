37c8ce0b59a52df6db6fbe48f54ec40b96c6c6be
BoYkostan <ilya.boyko.200021@mail.ru>
2019-05-20 01:10:58 +0700

    add board

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
-#include "board_start.h"
+#include "board_game.h"
 #include "board_print_plain.h"
 #include "board.h"
 int main() {
     char deck[8][8];
-    board_start(deck);
+    board_game(deck);
     while(1) {
         board_print_plain(deck);
         board(deck);
